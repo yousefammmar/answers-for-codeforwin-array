463b9aa1edd3411cce21f18c469ef3af24d333b1
yousefammmar <123825108+yousefammmar@users.noreply.github.com>
2024-11-24 08:23:52 +0300

    Add files via upload

---
+++ C program to declare, initialize, input and print array elements.c
@@ -0,0 +1,16 @@
+//C program to declare, initialize, input and print array elements
+#include <stdio.h>
+int main(){
+    int n,x,array[x];
+    int i=0;
+    printf("input size:");
+    scanf("%d",&x);
+    printf("input element");
+    scanf("%d",&n);
+    while(i<=(x-1)){
+        printf("output:%d\n",n);
+        n++;
+        i++;
+    }
+    return 0;
+}
